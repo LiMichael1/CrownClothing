9bce29a8ba916f6013ae85158abbd975c015d9c8
Michael Li <limichael1099419@gmail.com>
2022-11-11 15:04:09 -0800

    Implement Checkout Page and Cart Drop Down Menu

--- src/contexts/cart.context.js
+++ src/contexts/cart.context.js
@@ -1,14 +1,103 @@
-import { createContext, useState } from 'react';
+import { createContext, useState, useEffect } from 'react';
+
+export const addCartItem = (cartItems, productToAdd) => {
+  // Check if Cart Item exists
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === productToAdd.id
+  );
+
+  // Add one to existing Cart Item
+  if (existingCartItem) {
+    return cartItems.map((cartItem) => {
+      return cartItem.id === productToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem;
+    });
+  }
+
+  // Add original Cart Item
+  return [...cartItems, { ...productToAdd, quantity: 1 }];
+};
+
+const removeCartItem = (cartItems, cartItemToRemove) => {
+  // find the cart item to remove
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToRemove.id
+  );
+
+  // Remove Item from Cart if there's only one quantity
+  if (existingCartItem.quantity === 1) {
+    // Returns an array without that cart item
+    return cartItems.filter((cartItem) => cartItem.id !== cartItemToRemove.id);
+  }
+
+  // Return back Cart Items with matching cart item with reduced quantity
+  return cartItems.map((cartItem) => {
+    return cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem;
+  });
+};
+
+const clearCartItem = (cartItems, cartItemToClear) =>
+  cartItems.filter((cartItem) => cartItem.id !== cartItemToClear.id);
 
 export const CartContext = createContext({
   // initial state
   isCartOpen: false,
   setIsOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
+  cartCount: 0,
+  cartTotal: 0,
 });
 
 export const CartProvider = ({ children }) => {
   const [isCartOpen, setIsCartOpen] = useState(false);
-  const value = { isCartOpen, setIsCartOpen };
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
+
+  useEffect(() => {
+    // Add to Cart Item Count
+    const newCartCount = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity,
+      0
+    );
+
+    setCartCount(newCartCount);
+  }, [cartItems]);
+
+  useEffect(() => {
+    // Add up the Price
+    const newCartTotal = cartItems.reduce(
+      (total, cartItem) => total + cartItem.quantity * cartItem.price,
+      0
+    );
+    setCartTotal(newCartTotal);
+  }, [cartItems]);
+
+  const addItemToCart = (productToAdd) =>
+    setCartItems(addCartItem(cartItems, productToAdd));
+
+  const removeItemFromCart = (cartItemToRemove) =>
+    setCartItems(removeCartItem(cartItems, cartItemToRemove));
+
+  const clearItemFromCart = (cartItemToClear) =>
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
+
+  const value = {
+    isCartOpen,
+    setIsCartOpen,
+    addItemToCart,
+    removeItemFromCart,
+    clearItemFromCart,
+    cartItems,
+    cartCount,
+    cartTotal,
+  };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
 };
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,6 +4,7 @@ import { Outlet, Link } from 'react-router-dom';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
+
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 
 import { UserContext } from '../../contexts/user.context';
